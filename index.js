dbb0b6772e3e2a2c48167f049c5746688673cc8a
Guillermo Alfaro <galfarotolon@gmail.com>
2020-03-26 17:28:07 -0500

    finished all project without stretch questions.

--- index.js
+++ index.js
@@ -143,7 +143,36 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {}
+class Instructor extends Lambdasian {
+  constructor(object) {
+    super(object)
+    this.specialty = object.specialty,
+      this.favLanguage = object.favLanguage,
+      this.catchPhrase = object.catchPhrase
+  }
+  demo(subject) {
+    return `Today we are learning about ${subject}.`
+  }
+  grade(student, subject) {
+    return `${student.name}receives a perfect score on ${subject}`
+  }
+  // about() {
+  //   return `Hello, my name is ${this.name}, I am ${this.age} years old and I love ${this.specialty}. My
+  //   favorite language is ${this.favLanguage}. ${this.catchPhrase}!!!`
+  // }
+}
+
+// const instructorOne = new Instructor({
+//   name: "Guillermo",
+//   age: 26,
+//   location: "Peru",
+//   specialty: "coding",
+//   favLanguage: "Javascript",
+//   catchPhrase: "Wubba Lubba Dub Dub"
+
+// })
+
+// console.log(instructorOne.about());
 
 /*
   TASK 5
@@ -160,7 +189,27 @@ class Instructor {}
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {}
+class Student extends Lambdasian {
+  constructor(object) {
+    super(object)
+    this.previousBackground = object.previousBackground,
+      this.className = object.className,
+      this.favSubjects = object.favSubjects
+  }
+  listSubjects() {
+    return `Loving ${this.favSubjects}!`
+
+  }
+  PRAssignment(subject) {
+    return `${this.name} has begun sprint challenge on ${subject}.`
+
+  }
+
+  sprintChallenge(subject) {
+    return `${this.name} has begun sprint challenge on ${subject}.`
+
+  }
+}
 
 /*
   TASK 6
@@ -175,7 +224,20 @@ class Student {}
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {}
+class ProjectManager extends Instructor {
+  constructor(object) {
+    super(object)
+    this.gradClassName = object.gradClassName,
+      this.favInstructor = object.favInstructor
+  }
+  standUp(slackChannel) {
+    return `${this.name} announces to ${slackChannel}, @channel standy times!`
+
+  }
+  debugsCode(student, subject) {
+    return `${this.name} debugs ${student.name}'s code on ${subject}`
+  }
+}
 
 /*
   STRETCH PROBLEM (no tests!)
